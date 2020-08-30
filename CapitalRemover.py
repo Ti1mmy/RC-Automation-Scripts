ccd2c42bbd1ec76590556fa5a171de2acc455257
Ti1mmy <47431121+Ti1mmy@users.noreply.github.com>
2020-08-30 17:34:20 -0400

    yes

---
+++ CapitalRemover.py
@@ -0,0 +1,11 @@
+with open("cooldowns.conf", "r", encoding="utf8") as File1:
+    file_contents = File1.read().split('\n')
+newlist = []
+for line in file_contents:
+    if '=' in line:
+        newlist.append(line.lower())
+    else:
+        newlist.append(line)
+with open("output.conf", "a", encoding="utf-8") as output:
+    for line in newlist:
+        output.write(f'{line}\n')
